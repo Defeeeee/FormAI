beefb63120d7e42b393cffb67f0be241b23d2b93
Federico Diaz Nemeth <81602924+Defeeeee@users.noreply.github.com>
2024-07-25 13:25:57 -0300

    fix(CVision): Mac and continuity cameras issue

--- Computer Vision/Mediapipe pose detection/main.py
+++ Computer Vision/Mediapipe pose detection/main.py
@@ -46,35 +46,47 @@ mp_pose = mp.solutions.pose
 #     # Plot pose world landmarks.
 #     mp_drawing.plot_landmarks(
 #         results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
+# global successi
+successi = True
+
 
 # For webcam input:
-cap = cv2.VideoCapture(0)
-with mp_pose.Pose(
-        min_detection_confidence=0.5,
-        min_tracking_confidence=0.5) as pose:
-    while cap.isOpened():
-        success, image = cap.read()
-        if not success:
-            print("Ignoring empty camera frame.")
-            # If loading a video, use 'break' instead of 'continue'.
-            continue
+def main(successi=True):
+    if successi:
+        cap = cv2.VideoCapture(0)
+        print("Webcam 0")
+    else:
+        cap = cv2.VideoCapture(1)
+        print("Webcam 1")
+    with mp_pose.Pose(
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5) as pose:
+        while cap.isOpened():
+            success, image = cap.read()
+            if not success:
+                successi = True
+                main(successi)
 
-        # To improve performance, optionally mark the image as not writeable to
-        # pass by reference.
-        image.flags.writeable = False
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        results = pose.process(image)
+            # To improve performance, optionally mark the image as not writeable to
+            # pass by reference.
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = pose.process(image)
 
-        # Draw the pose annotation on the image.
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        mp_drawing.draw_landmarks(
-            image,
-            results.pose_landmarks,
-            mp_pose.POSE_CONNECTIONS,
-            landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-        # Flip the image horizontally for a selfie-view display.
-        cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
-        if cv2.waitKey(5) & 0xFF == 27:
-            break
-cap.release()
+            # Draw the pose annotation on the image.
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            mp_drawing.draw_landmarks(
+                image,
+                results.pose_landmarks,
+                mp_pose.POSE_CONNECTIONS,
+                landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+            # Flip the image horizontally for a selfie-view display.
+            cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
+    cap.release()
+
+
+if __name__ == '__main__':
+    main(successi)
