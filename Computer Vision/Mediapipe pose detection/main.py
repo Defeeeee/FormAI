06f5544afe471ede59ce0f33cc2d3ee05daa369a
Federico Diaz Nemeth <81602924+Defeeeee@users.noreply.github.com>
2024-07-26 13:24:44 -0300

    fix(CVision): Mac and continuity cameras issue

--- Computer Vision/Mediapipe pose detection/main.py
+++ Computer Vision/Mediapipe pose detection/main.py
@@ -77,12 +77,8 @@ def main(successi=True):
             image.flags.writeable = True
             image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
             mp_drawing.draw_landmarks(
-                image,
-                results.pose_landmarks,
-                mp_pose.POSE_CONNECTIONS,
-                landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-            # Flip the image horizontally for a selfie-view display.
-            cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
+                image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+            cv2.imshow('MediaPipe Pose', image)
             if cv2.waitKey(5) & 0xFF == 27:
                 break
     cap.release()
