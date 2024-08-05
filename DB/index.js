7a66b0e889ef73d6c117bfc3c470a75c40d8b98a
Eric Gerzenstein <112182259+elmUffins@users.noreply.github.com>
2024-08-05 14:06:49 -0300

    feat: npm dependencies + index setup

--- DB/index.js
+++ DB/index.js
@@ -1,1 +1,13 @@
-// bobi
+import express from "espress";
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (_, res) => {
+    res.send("1");
+});
+
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+});
