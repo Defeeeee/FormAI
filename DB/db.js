5b67bda3ace787b0b1fddde2f28a397d5735cab8
Eric Gerzenstein <112182259+elmUffins@users.noreply.github.com>
2024-08-07 08:58:07 -0300

    feat(pg): pg packash

--- DB/db.js
+++ DB/db.js
@@ -1,11 +1,14 @@
-import mysql from "mysql2/promise";
+import { Client } from "pg";
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const conn = await mysql.createConnection({
+export const client = new Client({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: 3000,
 });
+
+client.connect();
